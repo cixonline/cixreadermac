8ce89c54ab7d4be176a79f711e1d3fe5d03bfd75
Steve Palmer <steve@icuk.net>
2016-02-03 16:28:36 +0000

    Fix issue where scrollbars affect the popup position.

--- CIXReader/SharedSupport/Styles/Library/popup.js
+++ CIXReader/SharedSupport/Styles/Library/popup.js
@@ -1,9 +1,10 @@
 tooltipShow = function(event,item) {
     var e = item.getElementsByClassName('tooltip_text')[0];
     var l = event.clientX + 5;
+    var yOffset=Math.max(document.documentElement.scrollTop,document.body.scrollTop);
     if (l < 20) l = 20;
     if (l > window.innerWidth - 350) l = window.innerWidth - 350;
-    e.style.top = (event.clientY + 20) + 'px';
+    e.style.top = (event.clientY + 20 + yOffset) + 'px';
     e.style.left = l + 'px';
     e.style.display = 'block';
 }
